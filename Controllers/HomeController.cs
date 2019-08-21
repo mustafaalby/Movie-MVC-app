501e935aa11b354fecf4dea59213b49fc8815caf
mustafaalby <albymusty9561@gmail.com>
2019-08-21 17:18:41 +0300

    Some controller and views added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_MVC_app.Controllers
+{
+    public class HomeController:Controller
+    {
+        
+        public IActionResult index()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+        public IActionResult Contact()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+    }
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_MVC_app.Controllers
+{
+    public class MovieController:Controller
+    {
+        public IActionResult index()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+        public IActionResult Create()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+        public IActionResult Details()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+        public IActionResult List()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+    }
+}
